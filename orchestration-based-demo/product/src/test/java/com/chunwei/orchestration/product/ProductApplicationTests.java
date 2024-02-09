80beab4d312e99862b305b1c4a53c9f2491fc44e
Chun-Wei Chang <chunwei.chang.cwc@gmail.com>
2024-02-08 21:05:55 -0800

    feat: orchestration-based java package init

---
+++ orchestration-based-demo/product/src/test/java/com/chunwei/orchestration/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunwei.orchestration.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
